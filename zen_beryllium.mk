c543441501f994a58e705d819b3dd58a17a7c360
wirmpolter <wirmpolter@gmail.com>
2019-04-13 00:14:05 -0400

    beryllium: Initial Zen OS bring-up

--- zen_beryllium.mk
+++ zen_beryllium.mk
@@ -1,16 +1,16 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2019 ZenOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Zen OS stuff.
+$(call inherit-product, vendor/zen/configs/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := zen_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Poco F1
